4237db507102e6201e50a6e4a159ababa15fa13b
2025-0251-png <2025-0251@unad.edu.do>
2026-06-18 01:23:02 -0400

    Add files via upload

---
+++ mayor y menor/mayor y menor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("mayor y menor");
+
+int[] numeros = new int[10];
+for (int i = 0; i <= 9; i++)
+{
+    Console.Write($"ingresa los numeros {i} ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+for (int num = 0; num <= 9; num++)
+{
+    Console.WriteLine(numeros[num]);
+}
+ int mayor = numeros[0];
+ int menor = numeros[0];
+for (int i = 1; i <= 9; i++)
+{
+    if (numeros[i] > mayor) 
+    {
+        mayor = numeros[i];
+    }
+    if (numeros[i] < menor) 
+    {
+        menor = numeros[i];
+    }
+}
+Console.WriteLine($"el numero mayor es {mayor}");
+Console.WriteLine($"el numero menor es {menor}");
